4a8641f8d8aba5174ad4de5ab4fd67ed2dc5209a
Victor Burbano Rincon <vburbano@live.com>
2020-05-03 16:54:00 -0500

    deberia estar funcionando al pelo

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// Start
+// Inicio
 $(function () {
   juego();
 });
@@ -8,22 +8,11 @@ var puntuacion = movimientos = clicks = 0;
 
 function juego() {
   colorTitulo("h1.main-titulo");
-  //borrar los 2 de abajo cuando hagas el evento click del inicio para  que no comience sin darle click*****
-  randomSrc();
-  llenarTodos();
-  //$('.btn-reinicio').click(function () {
-  //	if ($(this).text() === 'Reiniciar') {
-  //		location.reload(true);
-  //	}
-  //	checkBoard();
-  //	$(this).text('Reiniciar');
-  //	$('#timer').startTimer({
-  //		onComplete: endGame
-  //	})
-  //	});
+  $("body").on('finTiempo', tiempoAcabo)
+  $('.btn-reinicio').on('click', ClickIniciar)
 }
-/*
-function firstClickIniciar(){
+
+function ClickIniciar(){
   clicks++;
   if(clicks==1){
     var tiempo = 60 * 2; //Minutos para el juego
@@ -35,7 +24,7 @@ function firstClickIniciar(){
     location.reload();
   }
 
-}*/
+}
 //punto 1. Funcion recursiva primero pinta blanco, espera y pinta amarillo para volver a llamarse asi misma. 
 function colorTitulo(selector) {
   $(selector).animate({ color: "white" }, "slow");
@@ -59,7 +48,6 @@ function sumarMovimiento(){
 
 function llenarElemento(columna, espacios){
   for (var i = 0; i < espacios; i++) {
-    //$(columna).prepend("<img src='"+randomSrc()+"' class='elemento'/>")
     var elemento = document.createElement("img");
     $(elemento)
       .attr("src", randomSrc())
@@ -83,7 +71,7 @@ function llenarElemento(columna, espacios){
     $(columna).prepend(elemento);
   }
 }
-//Función que llena todas las casillas de la cuadrícula para iniciar el juego
+//Incia llenando las casillas
 function llenarTodos(){
   var columna;
   for (var i = 1; i <= 7; i++) {
@@ -91,3 +79,152 @@ function llenarTodos(){
     llenarElemento($(columna), 7);
   }
 }
+
+
+/*
+**************************************************************************
+********************revisar***********************************************
+**************************************************************************
+*/
+
+
+//Función auxiliar que compara si dos elementos Dulce son iguales
+function checkSrc(elemento1, elemento2){
+  if ($(elemento1).attr("src")==$(elemento2).attr("src")) {
+    return true;
+  }else return false;
+}
+//Función que adiciona los puntos y esconde los elementos en línea
+function punto(elemento1, elemento2, elemento3){
+  puntuacion= puntuacion + 10;
+  $("#score-text").text(puntuacion);
+  $(elemento1).hide('pulsate', 2000)
+  $(elemento2).hide('pulsate', 2000)
+  $(elemento3).hide('pulsate', 2000)
+
+}
+//Función auxiliar que elimina los elementos escondidos de la estructura del DOM
+function eliminarElementos(){
+  $("img:hidden").each(function(index){
+      $(this).remove()
+  })
+}
+//Función que llena cada columna con los elementos faltantes
+function llenarDespuesTurno(){
+  var numeroElementos = numeroFalta = 0;
+  for (var i = 1; i <= 7; i++) {
+    numeroElementos=$(".col-"+i).find("img").length;
+    numeroFalta = 7 - numeroElementos;
+    llenarElemento($(".col-"+i), numeroFalta);
+  }
+  window.setTimeout(postJugada, 500)
+}
+//Función que recorre todo el tablero verificando si hay match de 3 dulces o mas, horizontal y verticalmente
+function checkMatch(){
+  var elementoCompara;
+  var actual;
+  var matchIzquierda = false;
+  var matchDerecha = false;
+  var matchAbajo = false;
+  var matchArriba = false;
+  for (var col = 1; col <= 7; col++) {
+    for (var row = 0; row < 7; row++) {
+      matchArriba=matchAbajo=matchDerecha=matchIzquierda=false;
+      actual = $(".col-"+col).find("img")[row]
+
+      //Verficacion a la Izquierda
+      if($(".col-"+(col-1)).length > 0){ //Verifica si existe elemento a la izquierda
+        elementoCompara = $(".col-"+(col-1)).find("img")[row]
+        if (checkSrc(actual, elementoCompara)) {
+          matchIzquierda = true;
+          if($(".col-"+(col-2)).length > 0){ //Verifica si existen dos columnas a la izquierda
+            elementoCompara = $(".col-"+(col-2)).find("img")[row]
+            if(checkSrc(actual, elementoCompara)){
+              punto(actual, $(".col-"+(col-1)).find("img")[row], elementoCompara )
+
+            }
+          }
+        }
+      }
+
+      //Verificacion a la Derecha
+      if($(".col-"+(col+1)).length > 0){ //Verifica si existe elemento a la izquierda
+        elementoCompara = $(".col-"+(col+1)).find("img")[row]
+        if (checkSrc(actual, elementoCompara)) {
+          matchDerecha = true;
+          if($(".col-"+(col+2)).length > 0){ //Verifica si existen dos columnas a la izquierda
+            elementoCompara = $(".col-"+(col+2)).find("img")[row]
+            if(checkSrc(actual, elementoCompara)){
+              punto(actual, $(".col-"+(col+1)).find("img")[row], elementoCompara )
+
+            }
+          }
+        }
+      }
+
+      //Verificacion ambos izquierda y Derecha
+      if (matchIzquierda == true && matchDerecha == true) {
+        punto(actual, $(".col-"+(col-1)).find("img")[row], $(".col-"+(col+1)).find("img")[row])
+
+      }
+      //Verificación hacia arriba
+      if($(".col-"+col).find("img")[row-1]){ //Verifica si existe elemento arriba
+        elementoCompara = $(".col-"+col).find("img")[row-1]
+        if (checkSrc(actual, elementoCompara)) {
+          matchArriba = true;
+          if($(".col-"+col).find("img")[row-2]){ //Verifica si existen dos filas hacia arriba
+            elementoCompara = $(".col-"+col).find("img")[row-2]
+            if(checkSrc(actual, elementoCompara)){
+              punto(actual, $(".col-"+col).find("img")[row-1], elementoCompara)
+
+            }
+          }
+        }
+      }
+
+      //Verificacion hacia abajo
+      if($(".col-"+col).find("img")[row+1]){ //Verifica si existe elemento abajo
+        elementoCompara = $(".col-"+col).find("img")[row+1]
+        if (checkSrc(actual, elementoCompara)) {
+          matchAbajo = true;
+          if($(".col-"+col).find("img")[row+2]){ //Verifica si existen dos filas hacia abajo
+            elementoCompara = $(".col-"+col).find("img")[row+2]
+            if(checkSrc(actual, elementoCompara)){
+              punto(actual, $(".col-"+col).find("img")[row+1], elementoCompara)
+
+            }
+          }
+        }
+      }
+      //Verificacion ambos Arriba y Abajo
+      if (matchArriba == true && matchAbajo == true) {
+        punto(actual, $(".col-"+col).find("img")[row+1], $(".col-"+col).find("img")[row-1])
+
+      }
+    }
+
+
+  }
+}
+//despues de mover haga
+function postJugada(){
+  checkMatch();
+  window.setTimeout(eliminarElementos,2100);
+  window.setTimeout(llenarDespuesTurno, 2200);
+
+}
+//Evento que reorganiza la página al acabarse el tiempo.
+function tiempoAcabo(){
+  $('.panel-tablero').hide(900);
+  $('.panel-score')
+    .animate({
+      width: '100%'
+    }, 1000, function(){
+      $(this).prepend("<h2 class='titulo-over'>Juego Terminado</h2>")
+    })
+  $('.time').hide(500)
+  $('#score-text').hide()
+  $('.score').append("<span class='data-info' id='score-final'>"+puntuacion+"</span>")
+}
+
+
